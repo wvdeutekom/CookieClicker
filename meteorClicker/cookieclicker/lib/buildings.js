a32db0c0113a306373a6994b57ffe317c088279d
Wijnand van Deutekom <wijnand@zerok.nl>
2015-02-12 21:30:08 +0100

    added buildings

--- meteorClicker/cookieclicker/lib/buildings.js
+++ meteorClicker/cookieclicker/lib/buildings.js
@@ -1,4 +1,34 @@
 Buildings = new Mongo.Collection("buildings");
 
+// 0.1
+// 0.5
+// 2.0
+// 5.0
+// 10.0
+// 50
 
 
+// var rolstoeler 
+
+// var invalide
+
+// var fabriek
+
+// var zwartGat
+
+// var jostiband
+
+
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+  	if(Buildings.find().count() === 0) {
+  		Buildings.insert({
+  			_id: "1",
+  			name: "rolstoeler",
+  			cost: 15,
+  			income: 0.1,
+  		});
+  	}
+  });
+}
--- meteorClicker/cookieclicker/lib/cookies.js
+++ meteorClicker/cookieclicker/lib/cookies.js
@@ -1,4 +1,5 @@
 Cookies = new Mongo.Collection("cookies");
+HasBuildings = new Mongo.Collection("hasBuildings");
 
 
 
@@ -9,7 +10,18 @@ if (Meteor.isServer) {
   		Cookies.insert({
   			_id: "COOKIE",
   			total: 0,
-  			current: 0
+  			current: 0,
+  			buildings: {
+  				collection: "hasBuildings",
+  				via: "cookieId"
+  			}
+  		});
+  	}
+  	if(HasBuildings.find().count() === 0) {
+  		HasBuildings.insert({
+  			cookieId: "COOKIE",
+  			buildingId: 1,
+  			amount: 1
   		});
   	}
   });
