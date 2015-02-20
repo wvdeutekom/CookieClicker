668725b4c19a0b764c6cf607d9748ec0701e62df
Tjeerd Ritsma <tjeerd@projectie.com>
2015-02-20 23:33:16 +0100

    remove income.js integrate its functionality as a method and call the method from squad on server.startup

--- server/publications.js
+++ server/publications.js
@@ -2,8 +2,9 @@ Meteor.methods({
 	hasBuildingsForID: function (buildingID) {
 	   return HasBuildings.findOne({ cookieId: "COOKIE", buildingId: parseInt(buildingID)}).amount;
 	},
-	createSquad: function() {
+	createSquad: function(name) {
 		var squad = {
+			name: name,
 			cookies_total: 0,
 			cookies_current: 0,
 			private: 0,
@@ -19,6 +20,23 @@ Meteor.methods({
 		});
 		Squad.insert(squad, function(error, id){
 			return id;
+		});
+	},
+	applyIncome: function() {
+		var amount = 0;
+		if(Meteor.user()){
+			var squad = Squad.findOne({_id: Meteor.user().squadId});
+		} else {
+			var squad = Squad.findOne({name: "Anonymous"});
+		}
+		for(var squadHasBuilding in squad.buildings) {
+		  	amount += Math.round(squadHasBuilding.amount * Buildings.findOne({_id: squadHasBuilding.buildingId}).income);
+		}
+		Squad.update({_id: squad._id},{
+			$inc: {
+				current: amount,
+				total: amount
+			}
 		});
 	}
 });
