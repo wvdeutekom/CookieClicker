e3a094a9deb2668fc7835c1253d721a2ebd34e0f
Tjeerd Ritsma <tjeerd@projectie.com>
2015-02-20 22:32:54 +0100

    Squad + building fixes

--- server/publications.js
+++ server/publications.js
@@ -1,5 +1,24 @@
 Meteor.methods({
 	hasBuildingsForID: function (buildingID) {
 	   return HasBuildings.findOne({ cookieId: "COOKIE", buildingId: parseInt(buildingID)}).amount;
+	},
+	createSquad: function() {
+		var squad = {
+			cookies_total: 0,
+			cookies_current: 0,
+			private: 0,
+			code: '',
+			admin: Meteor.userId(),
+			buildings: []
+		};
+		Buildings.find().forEach(function(building) {
+		  	squad.buildings.push({
+		  		buildingId: building._id,
+		  		amount: 0
+		  	});
+		});
+		Squad.insert(squad, function(error, id){
+			return id;
+		});
 	}
 });
